207e69ae746e21000358dcbf1475e2340317bccd
Juan David Marulanda V <jadamavecool@gmail.com>
2018-04-25 14:20:23 -0500

    test de SOAP y REST

---
+++ src/Controller/AppController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use \SoapClient;
+
+
+
+
+
+
+class AppController extends Controller
+{
+
+
+    /**
+     * @Route("/servicio/soap", name="servicioSoap")
+     */
+
+    public function soap(Request $request)
+    {
+
+        $url = "http://www.xmethods.net/sd/2001/EBayWatcherService.wsdl";
+        try {
+            $soap = new SoapClient($url,array('location'=>Endpoint,'trace'=>true,'exceptions'=>false));
+            $result = $soap->__soapCall();
+        } catch ( SoapFault $e ) {
+            echo $e->getMessage();
+        }
+
+        var_dump($result);
+        die();
+
+
+    }
+
+    /**
+     * @Route("/servicio/rest", name="servicioRest")
+     */
+
+    public function listaUsuario(Request $request)
+    {
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://reqres.in/api/users',
+        ));
+        $resp = json_decode(curl_exec($curl));
+        curl_close($curl);
+        var_dump($resp);
+        die();
+
+    }
+
+}
