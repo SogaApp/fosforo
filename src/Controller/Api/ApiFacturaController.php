1fa9caa4047e4851de1532a86fb0560125c0c990
wariox3 <maestradaz3@gmail.com>
2018-06-13 13:38:44 -0500

    Bundle fos rest

---
+++ src/Controller/Api/ApiFacturaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Factura;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ApiFacturaController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/api/factura/lista/{codigoFactura}", name="api_factura_lista")
+     */
+    public function lista(Request $request, $codigoFactura)
+    {
+        set_time_limit(0);
+        ini_set("memory_limit", -1);
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $qb->from(Factura::class, "f")
+            ->select("f.codigoFacturaPk")
+            ->where("f.codigoFacturaPk <> 0");
+        return $qb->getQuery()->getResult();
+    }
+
+
+}
