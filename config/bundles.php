54d0b81a1d8eb51c9c64c6c6d84d8772c38b5a88
Juan Felipe Mesa Ocampo <felipemesa14@gmail.com>
2018-09-11 16:49:01 -0500

    feat(Se agregan relaciones al momento de crear una factura):
    Se agregan relaciones al momento de crear una factura

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
